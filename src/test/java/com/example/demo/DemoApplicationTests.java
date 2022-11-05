31eab9d5d78af2eb81741c4fcc67851ffe29bc6a
ahmedbenissa <48155622+ahmedbenissa@users.noreply.github.com>
2022-11-05 03:28:19 +0100

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,10 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.example.demo.services.StockServiceImpl;
-
 @SpringBootTest
 class DemoApplicationTests {
-	StockServiceImpl ss;
 	@Test
 	void contextLoads() {
-		ss.retrieveAllStocks();
 	}
 
 }
